7c02715f7a63d0c02a2ed71ba517f2ba0d39bc9f
GermainDuran <arturodurantorres@gmail.com>
2019-01-16 10:55:57 -0500

    Done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,20 +1,27 @@
 import React from "react";
 import BotCard from "../components/BotCard";
 
-class BotCollection extends React.Component {
-  //your code here
+// I changed the class with const because it makes more sense to me in my mind wiht this way
+// I'm passing my robots and view from BotsPage thorugh the props
+//class BotCollection extends React.Component {
 
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
-  }
+const BotCollection = props => {
 
+const {robots,view } = props
+console.log(props,"Vene2")
+
+
+   const robot2= robots.map((robot) => {
+  	return <div className ="ui column"><BotCard bot={robot} view={view} /></div>
+  });
+
+   return (
+  <div className="ui four column grid">
+  	<div className='row'>
+  		{robot2}
+  	</div>
+  </div>
+  )
 };
 
-export default BotCollection;
+ export default BotCollection;
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,22 +1,35 @@
 import React from "react";
 import BotCard from "../components/BotCard";
 
-class YourBotArmy extends React.Component {
-  //your bot army code here...
 
-  render(){
-    return (
-      <div className="ui segment inverted olive bot-army">
+// I changed the class with const because it makes more sense to me in my mind wiht this way
+// I'm passing my robots, selectedRobots and view thorugh the props
+//class YourBotArmy extends React.Component {
+ const YourBotArmy = props => {
+
+  const {robots, selectedRobots, view} = props
+  console.log(props,"Vene3")
+
+   const robots2 = selectedRobots.map((botId,index) => {
+  	let foundBot = robots.find(robot => robot.id === botId)
+  	return (
+      <div key={index} className="ui three wide column">
+        <BotCard view={view} bot={foundBot}/>
+      </div>
+    )
+  });
+
+   return (
+
+       <div className='ui segment inverted olive bot-army'>
         <div className="ui five column grid">
-          <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
-          </div>
+        	<div className="row bot-army-row">
+        		{robots2}
+        	</div>
         </div>
       </div>
-    );
-  }
-  
+
+   )
 };
 
-export default YourBotArmy;
+ export default YourBotArmy;
