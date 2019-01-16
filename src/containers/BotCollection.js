e8ab32e18e01b49d2e9f8f87bef16f67ceb9bb02
Matt Migliore <mmigliore311@gmail.com>
2019-01-16 09:51:25 -0500

    init commit, add and remove bots to army

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -8,8 +8,13 @@ class BotCollection extends React.Component {
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {this.props.bots.map(bot => {
+            return <BotCard
+                     key={bot.id}
+                     bot={bot}
+                     recruitBot={this.props.recruitBot}
+                   />
+          })}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,53 @@
 import React from "react";
+import BotCollection from "./BotCollection"
+import YourBotArmy from "./YourBotArmy"
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state = {
+    bots: []
+  }
+
+  componentDidMount() {
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+    .then(r => r.json())
+    .then(data => {
+      const cleanedBots = data.map(bot => {
+        return {...bot, active: false}
+      })
+      this.setState({
+        bots: cleanedBots
+      })
+    })
+  }
+
+  recruitBot = (botId) => {
+    console.log("recruit:", botId)
+    const updatedBots = this.state.bots.map(bot => {
+      if (bot.id === botId) {
+        bot.active = !bot.active
+        return bot
+      } else {
+        return bot
+      }
+    })
+
+    this.setState({
+      bots: updatedBots
+    })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy
+          bots={this.state.bots.filter(bot => bot.active)}
+          recruitBot={this.recruitBot}
+        />
+        <BotCollection
+          bots={this.state.bots.filter(bot => !bot.active)}
+          recruitBot={this.recruitBot}
+        />
       </div>
     );
   }
