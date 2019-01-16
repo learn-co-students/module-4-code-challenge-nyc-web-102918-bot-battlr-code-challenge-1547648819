257dfb5d80c8375715a187e235e5449e1f634b16
leannemcabey <leanne.m.cabey@gmail.com>
2019-01-16 10:20:07 -0500

    first commit

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -8,8 +8,9 @@ class BotCollection extends React.Component {
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {this.props.allBots.map(bot => {
+            return <BotCard bot={bot} key={bot.id} handleBotCardClick={this.props.handleBotCardClick}/>
+          })}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React from "react"
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+  state = {
+    allBots: [],
+    botArmy: []
+  }
+
+  componentDidMount() {
+    fetch(' https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(result => result.json())
+    .then(allBots => this.setState({allBots}))
+  }
+
+  handleBotCardClick = (botId) => {
+    let enlisted = !!this.state.botArmy.find(bot => bot.id === botId)
+
+    if (!enlisted) {
+      let newRecruit = this.state.allBots.find(bot => bot.id === botId)
+      let allBotsCopy = [...this.state.allBots].filter(bot => bot.id !== botId)
+      this.setState({
+        allBots: allBotsCopy,
+        botArmy: [...this.state.botArmy, newRecruit]
+      })
+    }
+    else {
+      let dischargedBot = this.state.botArmy.find(bot => bot.id === botId)
+      let botArmyCopy = [...this.state.botArmy].filter(bot => bot.id !== botId)
+      this.setState({
+        allBots: [...this.state.allBots, dischargedBot],
+        botArmy: botArmyCopy
+      })
+    }
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy botArmy={this.state.botArmy} handleBotCardClick={this.handleBotCardClick}/>
+      <BotCollection allBots={this.state.allBots} handleBotCardClick={this.handleBotCardClick}/>
       </div>
     );
   }
