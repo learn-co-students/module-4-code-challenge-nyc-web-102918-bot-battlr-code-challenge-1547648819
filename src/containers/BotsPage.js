06dc0aabe5abc49cda2bcf2d29af47bf56ecdbc0
eric-klimowich <eklimowich@hotmail.com>
2019-01-16 10:06:33 -0500

    checkpoint reached

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,40 @@
 import React from "react";
+import BotCollection from './BotCollection'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+
+  state = {
+    robots: [],
+    myRobots: []
+  }
+
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(r => r.json())
+      .then(robots => this.setState({ robots }))
+  }
+
+  addBotToArmy = (id) => {
+    // console.log('Bot added')
+    // console.log(id)
+    const botToAdd = this.state.robots.find(bot => bot.id === id)
+    // console.log(botToAdd)
+    const myRobotsCopy = [...this.state.myRobots, botToAdd]
+    // console.log(myRobotsCopy)
+    this.setState({
+      myRobots: myRobotsCopy
+    })
+  }
+
   render() {
+    // console.log(this.state.robots)
+    // console.log(this.state.myRobots)
     return (
       <div>
-        {/* put your components here */}
+        <BotCollection robots={this.state.myRobots} />
+        <BotCollection robots={this.state.robots} addBotToArmy={this.addBotToArmy} />
       </div>
     );
   }
