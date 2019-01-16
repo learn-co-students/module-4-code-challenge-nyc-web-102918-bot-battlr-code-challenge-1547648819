b1a62cab32a2ea705aae23ef27ceeb030a538af4
EPICZEUS <s77broz@gmail.com>
2019-01-16 09:54:38 -0500

    Complete refactor step

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,25 +1,63 @@
 import React from "react";
 import BotCollection from './BotCollection'
 import YourBotArmy from './YourBotArmy'
+import BotSpecs from '../components/BotSpecs'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
   state = {
-    bots: []
+    bots: [],
+    selected: null
   }
 
   handleClick = id => {
   	const bot = this.state.bots.find(bot => bot.id === id)
 
-  	bot.owned = !bot.owned
+  	if (bot.owned) {
+  		bot.owned = false
 
-  	this.setState({ bots: this.state.bots })
+  		this.setState({ bots: this.state.bots })
+	  } else {
+	  	this.setState({ selected: id })
+	  }
+  }
+
+  handleEnlist = () => {
+  	const bot = this.state.bots.find(bot => bot.id === this.state.selected)
+
+  	bot.owned = true
+  	this.setState({ bots: this.state.bots, selected: null })
+  }
+
+  handleGoBack = () => {
+  	this.setState({ selected: null })
   }
 
   componentDidMount() {
     fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
       .then(r => r.json())
       .then(bots => this.setState({ bots }))
+  }
+
+  getComponent = () => {
+  	if (this.state.selected != null) {
+  		const bot = this.state.bots.find(bot => bot.id === this.state.selected)
+
+  		return (
+  			<BotSpecs
+  				bot={bot}
+  				handleEnlist={this.handleEnlist}
+  				handleGoBack={this.handleGoBack}
+  			/>
+  		)
+  	} else {
+  		return (
+  			<BotCollection 
+        	bots={this.state.bots.filter(bot => !bot.owned)}
+        	handleClick={this.handleClick}
+        />
+      )
+  	}
   }
 
   render() {
@@ -29,10 +67,7 @@ class BotsPage extends React.Component {
         	bots={this.state.bots.filter(bot => bot.owned)}
         	handleClick={this.handleClick}
         />
-        <BotCollection 
-        	bots={this.state.bots.filter(bot => !bot.owned)}
-        	handleClick={this.handleClick}
-        />
+        {this.getComponent()}
       </div>
     );
   }
