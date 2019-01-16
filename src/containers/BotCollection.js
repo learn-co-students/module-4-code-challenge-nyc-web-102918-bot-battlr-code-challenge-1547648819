20c34fdc5ac8f3f926f7587b18e109d5cf894de8
github username <github email address>
2019-01-16 10:56:45 -0500

    first commit

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -5,10 +5,11 @@ class BotCollection extends React.Component {
   //your code here
 
   render(){
+    // console.log(this.props)
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+    		  {this.props.bots.map( bot => <BotCard key= {bot.id} bot= {bot} addMyBot={this.props.addMyBot}/>)}
     		  Collection of all bots
     		</div>
   	  </div>
