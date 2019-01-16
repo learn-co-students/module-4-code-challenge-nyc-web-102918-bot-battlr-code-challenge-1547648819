69e680536fd3a4df61f43de56279969d6acf7b56
github username <github email address>
2019-01-16 10:20:21 -0500

    checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -3,12 +3,27 @@ import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
   //your code here
+  showBots = () => {
+    if (this.props.botCollection) {
+      return this.props.botCollection.map(bot => {
+        return <BotCard
+          bot={bot}
+          handleClick={this.props.handleClick}
+           />
+      })
+    } else {
+      return this.props.botArmy.map(bot => {
+        return <BotCard bot={bot} />
+      })
+    }
+  }
 
   render(){
+
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+    		  {this.showBots()}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,63 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+import BotCard from '../components/BotCard'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state = {
+    botCollection: [],
+    botArmy: []
+  }
+
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        botCollection: data
+      })
+    })
+  }
+
+  handleClick = (botID) => {
+    let selectedBot = this.state.botCollection.filter(bot => bot.id === botID)
+    let actualBot = selectedBot[0]
+    if (!this.state.botArmy.includes(actualBot)) {
+      this.setState({
+        botArmy: [...this.state.botArmy, actualBot]
+      })
+    }
+  }
+
+  // showBots = () => {
+  //   if (this.state.botCollection) {
+  //     return this.state.botCollection.map(bot => {
+  //       return <BotCard
+  //         bot={bot}
+  //         handleClick={this.handleClick}
+  //          />
+  //     })
+  //   } else {
+  //     return this.state.botArmy.map(bot => {
+  //       return <BotCard bot={bot} />
+  //     })
+  //   }
+  // }
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy
+          botArmy={this.state.botArmy}
+          showBots={this.showBots}
+           />
+        <BotCollection
+          botCollection={this.state.botCollection}
+          showBots={this.showBots}
+          handleClick={this.handleClick}
+           />
       </div>
     );
   }
