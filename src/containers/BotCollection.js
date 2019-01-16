bd9b0d7953aacade022a502a9991525cc10762c3
gbellini90 <gbellini90@gmail.com>
2019-01-16 10:21:42 -0500

    save checkpoint deliverables

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,15 +1,21 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import BotSpecs from "../components/BotSpecs";
 
 class BotCollection extends React.Component {
-  //your code here
+
 
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {this.props.botData.map(bot => {
+            return <BotCard
+            key={bot.id}
+            bot={bot}
+            handleClick={this.props.handleClick}
+            /> }
+          )}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,57 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+  state = {
+    botData:[],
+    clickedBotArray:[]
+  }
+
+  componentDidMount() {
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+    .then(r => r.json())
+    .then(botData=>{
+      this.setState({
+        botData
+      })
+    })
+  }
+
+  handleClick = (id) => {
+    console.log('clicked')
+    console.log(id)
+    let clickedBot = this.state.botData.find(robot => id === robot.id)
+    console.log(clickedBot)
+    if (!this.state.clickedBotArray.includes(clickedBot)){
+    let copyArray=[...this.state.clickedBotArray, clickedBot]
+    this.setState({
+      clickedBotArray:copyArray
+    })
+  }}
+
+  removeFromArmy = (id) => {
+    console.log("remove")
+    console.log(id)
+    let clickedBot = this.state.clickedBotArray.find(robot => id === robot.id)
+    let removeFromArray= this.state.clickedBotArray.filter(robot => id !== robot.id)
+    this.setState({
+      clickedBotArray:removeFromArray
+    })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+      <YourBotArmy
+      clickedBotArray={this.state.clickedBotArray}
+      removeFromArmy={this.removeFromArmy}
+      />
+      <BotCollection
+      botData={this.state.botData}
+      handleClick={this.handleClick}
+      />
       </div>
     );
   }
