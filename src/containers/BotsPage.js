f81455d3dc4a9eecb2e4ad60cd50c210d1fe8fde
leannemcabey <leanne.m.cabey@gmail.com>
2019-01-16 11:00:40 -0500

    complete

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,14 @@
 import React from "react"
 import BotCollection from './BotCollection'
 import YourBotArmy from './YourBotArmy'
+import BotSpecs from '../components/BotSpecs'
 
 class BotsPage extends React.Component {
 
   state = {
     allBots: [],
-    botArmy: []
+    botArmy: [],
+    detailViewBotId: null
   }
 
   componentDidMount() {
@@ -19,28 +21,55 @@ class BotsPage extends React.Component {
     let enlisted = !!this.state.botArmy.find(bot => bot.id === botId)
 
     if (!enlisted) {
-      let newRecruit = this.state.allBots.find(bot => bot.id === botId)
-      let allBotsCopy = [...this.state.allBots].filter(bot => bot.id !== botId)
-      this.setState({
-        allBots: allBotsCopy,
-        botArmy: [...this.state.botArmy, newRecruit]
-      })
+      this.showSpecs(botId)
     }
     else {
-      let dischargedBot = this.state.botArmy.find(bot => bot.id === botId)
-      let botArmyCopy = [...this.state.botArmy].filter(bot => bot.id !== botId)
+      this.dischargeBot(botId)
+    }
+  }
+
+  showSpecs = (botId) => {
+    let enlisted = !!this.state.botArmy.find(bot => bot.id === botId)
+
+    if (!enlisted) {
+      let newRecruit = this.state.allBots.find(bot => bot.id === botId)
       this.setState({
-        allBots: [...this.state.allBots, dischargedBot],
-        botArmy: botArmyCopy
+        detailViewBotId: newRecruit
       })
     }
+  }
+
+  enlistBot = (botId) => {
+    this.closeDetailView()
+    let newRecruit = this.state.allBots.find(bot => bot.id === botId)
+    let allBotsCopy = [...this.state.allBots].filter(bot => bot.id !== botId)
+    this.setState({
+      allBots: allBotsCopy,
+      botArmy: [...this.state.botArmy, newRecruit]
+    })
+  }
+
+  closeDetailView = () => {
+    this.setState({
+      detailViewBotId: null
+    })
+  }
+
+  dischargeBot = (botId) => {
+    let dischargedBot = this.state.botArmy.find(bot => bot.id === botId)
+    let botArmyCopy = [...this.state.botArmy].filter(bot => bot.id !== botId)
+    this.setState({
+      allBots: [...this.state.allBots, dischargedBot],
+      botArmy: botArmyCopy
+    })
   }
 
   render() {
     return (
       <div>
         <YourBotArmy botArmy={this.state.botArmy} handleBotCardClick={this.handleBotCardClick}/>
-      <BotCollection allBots={this.state.allBots} handleBotCardClick={this.handleBotCardClick}/>
+
+      {this.state.detailViewBotId ? <BotSpecs bot={this.state.detailViewBotId} closeDetailView={this.closeDetailView} enlistBot={this.enlistBot}/> : <BotCollection allBots={this.state.allBots} handleBotCardClick={this.handleBotCardClick}/>}
       </div>
     );
   }
