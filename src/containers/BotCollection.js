bad3c0bfcc9db22c483d6fde6d67de169fa868af
github username <github email address>
2019-01-16 11:00:45 -0500

    Esther's Code Challenge

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,11 +4,15 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
   //your code here
 
+
   render(){
+    // console.log(this.props)
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+          {this.props.bots.map (bot => {
+            return <BotCard bot={bot} addToArmy={this.props.addToArmy}/>
+          })}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -5,18 +5,21 @@ class YourBotArmy extends React.Component {
   //your bot army code here...
 
   render(){
+    console.log(this.props)
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
+          {this.props.yourBotArmy.map (bot => {
+            return <BotCard bot={bot} />
+          })}
             Your Bot Army
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
