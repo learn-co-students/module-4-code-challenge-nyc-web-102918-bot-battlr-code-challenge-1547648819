58bf468d359515e8f459d55f5c15837f73051d3c
Skylar Salerno <skylar.salerno@gmail.com>
2019-01-16 10:27:45 -0500

    reached checkpoint

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,37 @@
 import React from "react";
-
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  state= {
+      bots:[]
+  }
+
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(resp=>resp.json())
+    .then((bots)=>{
+      bots.forEach(bot => Object.assign(bot,{inArmy: true}))
+      this.setState({bots: bots})
+    })
+  }
+
+  toggleinArmy = (thisBot)=>{
+    let newBot= Object.assign({},thisBot, {inArmy: !thisBot.inArmy})
+    let newBots=this.state.bots.map(
+      bot=>{return bot.id===thisBot.id? newBot : bot}
+    )
+    this.setState({bots: newBots})
+  }
+
+  armyBots(){
+  return this.state.bots.filter(bot=>bot.inArmy)
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+      <YourBotArmy bots={this.armyBots()} handleBotClick={this.toggleinArmy}/>
+      <BotCollection bots={this.state.bots} handleBotClick={this.toggleinArmy}/>
       </div>
     );
   }
