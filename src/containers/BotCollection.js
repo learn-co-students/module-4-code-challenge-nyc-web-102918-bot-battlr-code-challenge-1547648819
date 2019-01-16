a0af421941edcc0b668771c3a2707c76fc6b6cfc
Connor Callahan <connorwcallahan@gmail.com>
2019-01-16 11:00:41 -0500

    blah

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,14 +2,35 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
+
+  state = {
+    enlisted: false
+  }
+
+  toggleEnlisted = (enlisted) => {
+    console.log(enlisted)
+    // if (enlisted === false) {
+    //   this.setState({
+    //     enlisted: true
+    //   })
+    // }
+    // else if(enlisted === true) {
+    //   this.setState({
+    //     enlisted: false
+    //   })
+    // }
+  }
+
 
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+          {
+            this.props.bots.map(bot => {
+              return <BotCard bot={{...bot}} handleAddBotClick={this.props.handleAddBotClick} enlisted={this.state.enlisted} toggleEnlisted={this.toggleEnlisted}/>
+            })
+          }
     		</div>
   	  </div>
   	);
