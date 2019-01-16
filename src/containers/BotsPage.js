47f4792e0bbb2a7362bd62ba6c2e6dc24ada0b48
Skylar Salerno <skylar.salerno@gmail.com>
2019-01-16 10:58:39 -0500

    Done

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,19 +1,30 @@
 import React from "react";
 import BotCollection from './BotCollection'
 import YourBotArmy from './YourBotArmy'
+import BotSpecs from '../components/BotSpecs'
 class BotsPage extends React.Component {
   state= {
-      bots:[]
+      bots:[],
+      showView: false,
+      ClickedBotId: 0
   }
 
   componentDidMount(){
     fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
     .then(resp=>resp.json())
     .then((bots)=>{
-      bots.forEach(bot => Object.assign(bot,{inArmy: true}))
+      bots.forEach(bot => Object.assign(bot,{inArmy: false}))
       this.setState({bots: bots})
     })
   }
+
+  selectBot=(id)=> {
+    this.setState(
+      Object.assign({}, this.state, {clickedBotId: id}, {showView: true})
+    )
+
+  }
+  exitShow= ()=>this.setState({showView:false})
 
   toggleinArmy = (thisBot)=>{
     let newBot= Object.assign({},thisBot, {inArmy: !thisBot.inArmy})
@@ -28,10 +39,19 @@ class BotsPage extends React.Component {
   }
 
   render() {
+    let botCollection=<BotCollection
+      bots={this.state.bots} handleBotClick={this.selectBot}
+      />
+    let botSpecs=<BotSpecs
+      bot={this.state.bots.find((bot)=>bot.id===this.state.clickedBotId)}
+      handleBotClick={this.selectBot}
+      toggleinArmy={this.toggleinArmy}
+      exitShow={this.exitShow}
+    />
     return (
       <div>
-      <YourBotArmy bots={this.armyBots()} handleBotClick={this.toggleinArmy}/>
-      <BotCollection bots={this.state.bots} handleBotClick={this.toggleinArmy}/>
+      <YourBotArmy bots={this.armyBots()} handleBotClick={this.selectBot}/>
+      {this.state.showView? botSpecs : botCollection}
       </div>
     );
   }
